87b3a9b9225072efa4197c2bd024380342ba694a
tglide <26071571+TGlide@users.noreply.github.com>
2024-02-27 00:18:17 +0000

    componentization init

---
+++ componentization/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '@unocss/reset/eric-meyer.css';
+	import '../app.css';
+	import 'virtual:uno.css';
+</script>
+
+<main class="min-h-screen bg-slate-900 text-white p-4">
+	<slot />
+</main>
---
+++ componentization/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<hgroup>
+	<h1 class="text-4xl font-semibold">Melty components</h1>
+	<h2 class="font-bold text-lg text-slate-300">
+		Examples of accessible components, built with Melt UI
+	</h2>
+	<h3 class="text-sm mt-1 text-slate-400">
+		Source code <a class="underline hover:opacity-75" href="https://github.com/melt-ui/examples"
+			>here</a
+		>.
+	</h3>
+</hgroup>
