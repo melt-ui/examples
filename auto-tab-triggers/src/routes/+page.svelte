be925e913f6cd71d72d35206fc15cb20e153bd39
tglide <26071571+TGlide@users.noreply.github.com>
2024-05-29 15:29:50 +0100

    add auto tab triggers example

---
+++ auto-tab-triggers/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Tab from '$lib/tab.svelte';
+	import Tabs from '$lib/tabs.svelte';
+</script>
+
+<Tabs>
+	<Tab tab="tab1">Tab 1</Tab>
+	<Tab tab="tab2">Tab 2</Tab>
+	<Tab tab="tab3">Tab 3</Tab>
+</Tabs>
